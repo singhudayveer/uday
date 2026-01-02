71c1cb760741bc9ee989f4b3b7ba0183bb0ef399
singhudayveer <singh2uday85@gmail.com>
2026-01-02 14:53:54 +0000

    second commit

---
+++ f1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+######################
+# Author  uday
+# date  02 jan 2026
+# Print even and odd number
+#######################
+#This is my commit
+for ((i=1;i<=10;i++))
+do
+	if ((i%2==0));then
+	echo "$i is even number"
+        else
+	echo "$i is odd number"
+
+	fi
+done	
